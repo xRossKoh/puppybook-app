ed0044e3efe4e49d98a70627e73fd3c4a226a56b
vanshiqa <e0411071@u.nus.edu>
2021-05-24 22:18:49 +0800

    Committing before changing to stable channel

---
+++ lib/constants.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/cupertino.dart';
+
+class Constants {
+  static const ROSEGOLD = Color(0xB76E79);
+}
--- lib/screens/homeScreen.dart
+++ lib/screens/homeScreen.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -7,9 +9,19 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        color: Colors.orange,
-        child: Text("Welcome to your favourite app!"),
+      appBar: AppBar(
+        title: Text("PuppyBook"), //CENTER THIS
+      ),
+      //TODO: ADD BACKGROUND COLOUR
+      body: Image(
+        image: AssetImage('images/MicrosoftTeams-image.png'),
+      ),
+
+      //   NetworkImage(
+      //       'https://www.rd.com/wp-content/uploads/2019/01/shutterstock_673465372.jpg?resize=768,512'),
+      // ), //TODO: Add asset image here
+      floatingActionButton: RaisedButton(
+        onPressed: () {},
       ),
     );
   }
